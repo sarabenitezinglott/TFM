69fb6fa96699a339fdd9d9dd02f469063b4abce0
sarabenitezinglott <saraab275@gmail.com>
2023-12-19 19:28:28 +0100

    new changes

--- Microscopy/src/exploratory.py
+++ Microscopy/src/exploratory.py
@@ -1,20 +1,35 @@
-from aicsimageio import AICSImage
-from aicsimageio.writers import OmeTiffWriter
+import numpy as np
+from aicspylibczi import CziFile
+from pathlib import Path
+import matplotlib.pyplot as plt
 
-def openczi(path, channels):
-    img = AICSImage(path)
-    img_data = img.data
-    print(img.dims)
-    print(img.shape)
+def openczi(path):
+    czi = CziFile(path)
+    return czi
 
-    first_channel_data = img.get_image_data(channels, C=1, S=0, T=0)
-    return img, first_channel_data
+def czimetadata(czi):
+    dimensions = czi.get_dims_shape() 
+    print(czi.dims)
+    print(czi.size)
+    img, shp = czi.read_image(S=7, Z=0)
+    return dimensions, img, shp
 
-def totiff(path, img, first_channel_data):
-    with OmeTiffWriter(path) as writer:
-        writer.save(first_channel_data, pixels_physical_size=img.get_physical_pixel_size(),
-                    dimension_order="ZYX")
 
-    channel_names = img.get_channel_names()
-    return channel_names
+def norm_by(x, min_, max_):
+    norms = np.percentile(x, [min_, max_])
+    i2 = np.clip((x - norms[0]) / (norms[1] - norms[0]), 0, 1)
+    return i2
 
+def recolor(im):  # transform from rgb to cyan-magenta-yellow
+    im_shape = np.array(im.shape)
+    color_transform = np.array([[1, 1, 0], [0, 1, 1], [1, 0, 1]]).T
+    im_reshape = im.reshape([np.prod(im_shape[0:2]), im_shape[2]]).T
+    im_recolored = np.matmul(color_transform.T, im_reshape).T
+    im_shape[2] = 3
+    im = im_recolored.reshape(im_shape)
+    return im
+
+def img_plot(cmy):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cmy)
+    plt.axis('off')
