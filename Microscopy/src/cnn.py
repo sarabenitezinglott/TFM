7474e4e35878738a2d2dbacc1afa0d8160ea9d46
sarabenitezinglott <saraab275@gmail.com>
2024-02-26 16:27:45 +0100

    pytorch

--- Microscopy/src/cnn.py
+++ Microscopy/src/cnn.py
@@ -14,6 +14,11 @@ from skimage import morphology, filters
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, Concatenate, Activation, BatchNormalization
 
+# import torch
+# from torch.nn import Conv2D, MaxPooling2D, MaxPool2d, Module, ModuleList, ReLU, Sequential, ConvTranspose2d
+# from torchvision.transforms import CenterCrop
+# from torch.nn import functional as F
+
 # 1. Mask generation for the U-net model 
 
 def mask(path, filetype):
@@ -186,7 +191,7 @@ def fitting(model, train_generator, valid_generator, weightpath, tensorboard):
                 TensorBoard(log_dir = tensorboard), checkpoints]
     steps_per_epoch = 60
     history = model.fit(train_generator, steps_per_epoch=steps_per_epoch, 
-                    validation_data = valid_generator, validation_steps=6, epochs = 6,
+                    validation_data = valid_generator, validation_steps=6, epochs = 4,
                     callbacks= callbacks, verbose = 1)
     return history
 
@@ -249,3 +254,79 @@ def nuclei_segmentation(folder):
     df.drop(columns=["Image_path"], inplace=True)
 
     return df
+
+################ OPTIMIZE THE MODEL --> Pytorch
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return x
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(EncoderBlock, self).__init__()
+        self.conv_block = ConvBlock(in_channels, out_channels)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        p = self.maxpool(x)
+        return x, p
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.conv_transpose = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv_block = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x, skip):
+        x = self.conv_transpose(x)
+        x = torch.cat([x, skip], dim=1)
+        x = self.conv_block(x)
+        return x
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+        self.encoder1 = EncoderBlock(in_channels, 64)
+        self.encoder2 = EncoderBlock(64, 128)
+        self.encoder3 = EncoderBlock(128, 256)
+        self.encoder4 = EncoderBlock(256, 512)
+        self.bridge = ConvBlock(512, 1024)
+        self.decoder1 = DecoderBlock(1024, 512)
+        self.decoder2 = DecoderBlock(512, 256)
+        self.decoder3 = DecoderBlock(256, 128)
+        self.decoder4 = DecoderBlock(128, 64)
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        s1, p1 = self.encoder1(x)
+        s2, p2 = self.encoder2(p1)
+        s3, p3 = self.encoder3(p2)
+        s4, p4 = self.encoder4(p3)
+        b1 = self.bridge(p4)
+        d1 = self.decoder1(b1, s4)
+        d2 = self.decoder2(d1, s3)
+        d3 = self.decoder3(d2, s2)
+        d4 = self.decoder4(d3, s1)
+        output = self.final_conv(d4)
+
+        return torch.sigmoid(output)
+
+
