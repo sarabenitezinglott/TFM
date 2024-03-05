8bda9772fec4eb94b5aa8a4cd79fe3e40ca50db3
sarabenitezinglott <saraab275@gmail.com>
2024-03-05 17:54:59 +0100

    new

--- RNA-Seq/SpliceQ.py
+++ RNA-Seq/SpliceQ.py
@@ -1,14 +1,9 @@
-import bamnostic as bs
-file = "D:\\TFM\\RNAseq\\BAM files\\WT41.Aligned.sortedByCoord.out.bam" 
-bam = bs.AlignmentFile(file, "rb")
-# 1. Get the attributes of bam 
-print(dir(bam))
-# 2. The data at the heather of bam
-print(bam.header)
-# 3. Next aligment
-align = next(bam)
-print(align)
-# 4. Loop inside bam to get all the information
+import pysam
+
+file =  "RNA-Seq/WT41.Aligned.sortedByCoord.out.bam"
+bamfile = pysam.AlignmentFile(file, "rb")
+print(bamfile.count)
+
 
 
 
