e61d1afc4319f2063994a42f2f45c1f1869f60ad
sarabenitezinglott <saraab275@gmail.com>
2024-03-06 11:56:09 +0100

    ubuntu

--- RNA-Seq/SpliceQ.py
+++ RNA-Seq/SpliceQ.py
@@ -1,9 +1,33 @@
+''' RUN IN UBUNTU
 import pysam
-
+import pandas as pd
+# The splicing efficiency can be measured in different ways, such us: 
 file =  "RNA-Seq/WT41.Aligned.sortedByCoord.out.bam"
-bamfile = pysam.AlignmentFile(file, "rb")
-print(bamfile.count)
 
+
+read_id = []
+chromosome = []
+position = []
+map_qual = []
+read_seq = []
+ 
+# 1. BAM file
+with pysam.AlignmentFile(file, "rb") as bamfile:
+    head = bamfile.header
+    print(head)
+    for alignment in bamfile:
+        read_id.append(alignment.query_name)
+        chromosome.append(alignment.reference_name)
+        position.append(alignment.reference_start)
+        map_qual.append(alignment.mapping_quality)
+        read_seq.append(alignment.query_sequence)
+
+# 2. Dataframe creation
+dictionary = {"read": read_id, "reference_name": chromosome, "position": position, "mapping_quality": map_qual, "sequence": read_seq}
+rnaseq_info = pd.DataFrame(dictionary)
+
+# print(rnaseq_info)
+'''
 
 
 
